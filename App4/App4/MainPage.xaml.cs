8ba93c93e29d6ef6ddd3b2f4e95be418b0355974
Supun Sarachitha Liyanaarachchi <36870488+supunsarachitha@users.noreply.github.com>
2018-11-19 23:02:55 +0530

    version 1

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        Stopwatch stopwatch;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            stopwatch = new Stopwatch();
+
+            lblStopwatch.Text = "00:00:00.00000";
+
+        }
+
+        private void btnStart_Clicked(object sender, EventArgs e)
+        {
+            if (!stopwatch.IsRunning)
+            {
+                stopwatch.Start();
+
+                Device.StartTimer(TimeSpan.FromMilliseconds(100), () =>
+                {
+                    lblStopwatch.Text = stopwatch.Elapsed.ToString();
+
+                    if (!stopwatch.IsRunning)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                    
+                }
+
+                );
+            }
+
+            
+           
+        }
+
+        private void btnStop_Clicked(object sender, EventArgs e)
+        {
+            btnStart.Text = "Resume";
+            stopwatch.Stop();
+        }
+
+        private void btnReset_Clicked(object sender, EventArgs e)
+        {
+            lblStopwatch.Text = "00:00:00.00000";
+            btnStart.Text = "Start";
+            stopwatch.Reset();
+        }
+    }
+}
